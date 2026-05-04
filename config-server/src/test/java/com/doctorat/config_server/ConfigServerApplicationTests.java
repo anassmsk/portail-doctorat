49486768f60a0bd06f9e5a7538b20a290aaa96c2
Anass Maskine <anassmskine99@gmail.com>
2026-05-04 21:50:29 +0100

    feat: ajout config-server et eureka-server

---
+++ config-server/src/test/java/com/doctorat/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctorat.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
